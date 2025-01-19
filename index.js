8f480c026f2de5ac556e6154ddde3ce7f90ea39f
Astrick004 <152830139+Astrick004@users.noreply.github.com>
2025-01-18 21:50:53 -0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename;
+
+    if (q.pathname === '/index') {
+        filename = './index.html';
+    } else if (q.pathname === '/about') {
+        filename = './about.html';
+    } else if (q.pathname === '/contact-me') {
+        filename = './contact-me.html';
+    } else {
+        filename = './404.html'; 
+    }
+
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/html' });
+            return res.end('500 Server Error');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+
